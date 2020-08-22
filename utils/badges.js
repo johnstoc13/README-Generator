e66f051e87adadbd41d79a6e0e831b2685fbc64b
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-22 09:33:42 -0400

    badge links added

---
+++ utils/badges.js
@@ -0,0 +1,11 @@
+// Badge links
+// Credit:  https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba#apache-20-license
+const badgeLinks = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    "Mozilla Public 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+}
+
+// Export badges
+module.exports = badgeLinks;
