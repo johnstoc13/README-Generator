464e264d07fd306bef5095107950e242d71d2c50
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-22 15:52:30 -0400

    added remaining readme items

--- utils/markdown.js
+++ utils/markdown.js
@@ -4,14 +4,43 @@ const badges = require("./badges");
 function generateAnswers(data) {
     data.badge = badges[data.license];
     return `
-    # ${data.title}
+# ${data.title}
 
-    ${data.badge}
+${data.badge}
 
-    ## Description
+## Description
+${data.description}
 
-    ${data.description}
-    `
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+${data.license}
+
+## Questions
+
+If you have any questions, please contact me at [${data.email}](mailto:${data.email}).\n
+Check out my GitHub [profile!](https://github.com/${data.username})\n
+©2020 ${data.fullname}`
 }
 
+// Export answers
 module.exports = generateAnswers;
